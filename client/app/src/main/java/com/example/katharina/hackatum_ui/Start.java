f887ddb15de8a4f82c6a65cde2059ad6ce245bf3
Katharina <ga83pad@mytum.de>
2017-11-18 13:09:40 +0100

    ...

--- client/app/src/main/java/com/example/katharina/hackatum_ui/Start.java
+++ client/app/src/main/java/com/example/katharina/hackatum_ui/Start.java
@@ -3,6 +3,7 @@ package com.example.katharina.hackatum_ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class Start extends AppCompatActivity {
@@ -11,7 +12,14 @@ public class Start extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        final int abTitleId = getResources().getIdentifier("action_bar_title", "id", "android");
+        findViewById(abTitleId).setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                //Do something
+            }
+        });
     }
 
     public void setTime(String time) {
