b37d8b17a7a3cfebe556bef8bc6029e73d7990a9
Katharina <ga83pad@mytum.de>
2017-11-18 11:59:54 +0100

    .gitignore for client

--- client/app/src/main/java/com/example/katharina/hackatum_ui/Start.java
+++ client/app/src/main/java/com/example/katharina/hackatum_ui/Start.java
@@ -1,7 +1,9 @@
 package com.example.katharina.hackatum_ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Start extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class Start extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+    }
+
+    public void setTime(String time) {
+        TextView txt = findViewById(R.id.textView6);
+        txt.setText(time);
+    }
+
+    public void setTableNumber(int number) {
+        TextView txt = findViewById(R.id.tableNum);
+        txt.setText(number);
     }
 }
