3fe46c7a540fcae1ed1acee4fc90948ff398a05f
Katharina <ga83pad@mytum.de>
2017-11-19 00:41:11 +0100

    import fixed

--- client/app/src/main/java/com/example/katharina/hackatum_ui/Question.java
+++ client/app/src/main/java/com/example/katharina/hackatum_ui/Question.java
@@ -1,6 +1,7 @@
 package com.example.katharina.hackatum_ui;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
