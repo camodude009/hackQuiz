dbf8e065e5627842a94ca7f95df6d7b1fc48f81b
Felix <felix.ma.fischer@gmail.com>
2017-11-18 03:23:24 +0100

    starting to change server

--- server/src/Registration.java
+++ server/src/Registration.java
@@ -8,10 +8,8 @@ import java.net.Socket;
 
 public class Registration extends Thread {
 
-    public void run(){
+    public void run() {
         Log.log("starting registration thread...");
-        long init_time = System.currentTimeMillis();
-        long registration_duration = 1000 * 10 * 60;
 
         Log.log("creating server socket...");
         ServerSocket serverSocket = null;
@@ -33,25 +31,12 @@ public class Registration extends Thread {
                     try {
                         BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
                         String register = in.readLine();
-                        String packetNr = register.substring(0, 3);
-                        String content = register.substring(3);
-                        Log.log("packetNr:" + packetNr);
-                        Log.log("content:" + content);
-
-                        if (register.startsWith("usr") && System.currentTimeMillis() < init_time + registration_duration) {
-                            Person p = null;
-                            synchronized (Main.persons) {
-                                Main.persons.add(p);
-                            }
-                            Log.log("registered" + p.toString());
-                        } else if (register.startsWith("tbl") && System.currentTimeMillis() >= init_time + registration_duration) {
-                            Table t = null;
-                            synchronized (Main.tables) {
-                                Main.tables.remove(t);
-                                Main.tables.add(t);
-                            }
-                            Log.log("registered" + t.toString());
+                        Table t = null; //TODO: gson
+                        synchronized (Main.tables) {
+                            Main.tables.remove(t);
+                            Main.tables.add(t);
                         }
+                        Log.log("registered" + t.toString());
                     } catch (IOException e) {
                         Log.log(e.getMessage());
                     }
